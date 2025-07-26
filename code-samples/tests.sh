f1d0db299cd95a016c9b2e7e572b908a679e9052
Flox <tosha.iljin55@gmail.com>
2025-07-26 10:54:29 +0000

    task 4

---
+++ code-samples/tests.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+declare -i counter=0
+
+check() {
+  local output=$(./DO "$1")
+  
+  if [[ "$output" == "$2" ]]; then
+    echo -e "Test "$counter" \e[32m pass\e[0m"
+  else
+    echo -e "Test "$counter" \e[31m failure\e[0m"
+  fi
+  ((counter++))
+}
+
+check "-2" "Bad number!"
+check "0.23" "Bad number!"
+check "23" "Bad number!"
+
+check "kqweljqwe" "Bad number!"
+
+check "1" "Learning to Linux"
+check "2" "Learning to work with Network"
+check "3" "Learning to Monitoring"
+check "4" "Learning to extra Monitoring"
+check "5" "Learning to Docker"
+check "6" "Learning to CI/CD"
