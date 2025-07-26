d973622541bcedf697716a4b6fe3606998fbff1c
Flox <tosha.iljin55@gmail.com>
2025-07-26 11:44:49 +0000

    asdasd

--- code-samples/tests.sh
+++ code-samples/tests.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 declare -i counter=0
+declare -i failedCount=0
 
 check() {
   local output=$(./DO "$1")
@@ -9,11 +10,12 @@ check() {
     echo -e "Test "$counter" \e[32m pass\e[0m"
   else
     echo -e "Test "$counter" \e[31m failure\e[0m"
+    ((failedCount++))
   fi
   ((counter++))
 }
 
-check "-2" "Bad number!"
+check "-2" "asjdlahsdjasdh"
 check "0.23" "Bad number!"
 check "23" "Bad number!"
 
@@ -25,3 +27,7 @@ check "3" "Learning to Monitoring"
 check "4" "Learning to extra Monitoring"
 check "5" "Learning to Docker"
 check "6" "Learning to CI/CD"
+
+if [ failedCount -gt 0 ]; then
+  exit 1 
+fi
