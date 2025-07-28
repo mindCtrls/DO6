db8137d962360471c16873b36ed16107fbce0746
Flox <tosha.iljin55@gmail.com>
2025-07-28 16:24:15 +0000

    Task 5 done?

--- code-samples/copy.sh
+++ code-samples/copy.sh
@@ -5,9 +5,18 @@ user=thk
 tmp=/tmp
 path=/usr/local/bin
 
-whoami
+if ! scp -o StrictHostKeyChecking=no -o ConnectTimeout=10 "$file" "$user@$url:$tmp"; then
+    echo -e "\e[31m SCP error\e[0m" >&2
+    exit 1
+fi
 
-scp -o StrictHostKeyChecking=no DO $user@$url:$tmp
-ssh -o StrictHostKeyChecking=no -tt $user@$url \ "sudo --non-interactive /bin/mv /tmp/DO /usr/local/bin/"
+if ! ssh -o StrictHostKeyChecking=no -o ConnectTimeout=10 -tt "$user@$url" \
+    "sudo --non-interactive /bin/mv -v '$tmp/$file' '$path/'"; then
+    echo -e "\e[31m SSH error\e[0m" >&2
+    exit 1
+fi
+
+echo -e "\e[32m Job SUCCESS  ))\e[0m"
+exit 0
 
 exit 0
