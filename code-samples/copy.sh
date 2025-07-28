d994950a57d61d7142ac412f91dd4900d282888a
Flox <tosha.iljin55@gmail.com>
2025-07-28 15:44:01 +0000

    see user

--- code-samples/copy.sh
+++ code-samples/copy.sh
@@ -5,10 +5,7 @@ user=thk
 tmp=/tmp
 path=/usr/local/bin
 
-
-echo start
-pwd
-ls
+whoami
 
 scp -o StrictHostKeyChecking=no DO $user@$url:$tmp
 ssh -o StrictHostKeyChecking=no -tt $user@$url \ "sudo --non-interactive /bin/mv /tmp/$1 /usr/local/bin/"
