2362f8233118b0d8eb6b02c522d0eb8a5e0afd0c
Flox <tosha.iljin55@gmail.com>
2025-07-28 08:30:15 +0000

    asd

--- code-samples/copy.sh
+++ code-samples/copy.sh
@@ -6,6 +6,11 @@ tmp=/tmp
 path=/usr/local/bin
 
 
+echo start
+pwd
+ls
+ping 192.168.0.20
+
 scp $1 $user@$url:$tmp
 ssh -o StrictHostKeyChecking=no -tt $user@$url \ "sudo --non-interactive /bin/mv /tmp/$1 /usr/local/bin/"
 
