90105cff5ca0f0df56e7afa6c6ae7c0e7dc10252
Flox <tosha.iljin55@gmail.com>
2025-07-31 06:05:57 +0000

    task 6 done?

--- code-samples/notify.sh
+++ code-samples/notify.sh
@@ -4,7 +4,7 @@ TELEGRAM_USER_ID=580298300
 
 sometext=qwe
 
-if [[ $CI_JOB_STATUS == "success" ]]; then 
+if [[ "$CI_JOB_STATUS" == "success" ]]; then 
   sometext="Этап $1 успешно пройден"
 else
   sometext="Этап $1 не пройден"
@@ -13,4 +13,4 @@ fi
 URL="https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage"
 TEXT="Deploy status: $sometext%0A%0AProject:+$CI_PROJECT_NAME%0AURL:+$CI_PROJECT_URL/pipelines/$CI_PIPELINE_ID/%0ABranch:+$CI_COMMIT_REF_SLUG"
 
-curl -s -d "chat_id=$TELEGRAM_USER_ID&disable_web_page_preview=1&text=$TEXT" $URL > /dev/null
+curl -s -d "chat_id=$TELEGRAM_USER_ID&disable_web_page_preview=1&text=$TEXT" "$URL" > /dev/null
