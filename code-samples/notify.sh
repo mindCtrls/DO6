41823b6579d66d5bd6170c302d7306cbc691056e
Flox <tosha.iljin55@gmail.com>
2025-07-31 05:47:30 +0000

    task 6

--- code-samples/notify.sh
+++ code-samples/notify.sh
@@ -2,7 +2,13 @@ TELEGRAM_BOT_TOKEN="8466818132:AAHvWsKO4HPz_o4tYD-quBWTP2EcsXlJNg0"
 TELEGRAM_USER_ID=580298300
 
 
+if [[ $CI_JOB_STATUS = "success"]]; then 
+  sometext= "Этап $1 успешно пройден"
+else
+  sometext= "Этап $1 не пройден"
+fi
+
 URL="https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage"
-TEXT="Deploy status: $1%0A%0AProject:+$CI_PROJECT_NAME%0AURL:+$CI_PROJECT_URL/pipelines/$CI_PIPELINE_ID/%0ABranch:+$CI_COMMIT_REF_SLUG"
+TEXT="Deploy status: $sometext%0A%0AProject:+$CI_PROJECT_NAME%0AURL:+$CI_PROJECT_URL/pipelines/$CI_PIPELINE_ID/%0ABranch:+$CI_COMMIT_REF_SLUG"
 
 curl -s -d "chat_id=$TELEGRAM_USER_ID&disable_web_page_preview=1&text=$TEXT" $URL > /dev/null
