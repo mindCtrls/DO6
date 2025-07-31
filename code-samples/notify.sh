cc8f17a381ade21f98720dad2245180fdedb4578
Flox <tosha.iljin55@gmail.com>
2025-07-31 05:34:52 +0000

    task 6

---
+++ code-samples/notify.sh
@@ -0,0 +1,8 @@
+TELEGRAM_BOT_TOKEN="8466818132:AAHvWsKO4HPz_o4tYD-quBWTP2EcsXlJNg0"
+TELEGRAM_USER_ID=580298300
+
+
+URL="https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage"
+TEXT="Deploy status: $1%0A%0AProject:+$CI_PROJECT_NAME%0AURL:+$CI_PROJECT_URL/pipelines/$CI_PIPELINE_ID/%0ABranch:+$CI_COMMIT_REF_SLUG"
+
+curl -s -d "chat_id=$TELEGRAM_USER_ID&disable_web_page_preview=1&text=$TEXT" $URL > /dev/null
